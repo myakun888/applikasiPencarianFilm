43373f1d2738d364aef7f1608515d619c4cf5572
muhammad fajrin <fajrinsaputra7@gmail.com>
2023-09-11 12:52:27 +0700

    menambahkan fitur detail di dalam halaman

--- object.mjs
+++ object.mjs
@@ -14,34 +14,74 @@ class api {
       .then((response) => {
         let film = response.Search;
         // console.log(film);
-        let x=""
-        film.forEach(e => {
-          x+= `<div class="card mt-3 me-1" style="width: 16rem">
-          <img src="${e.Poster}" class="card-img-top" alt="..." />
-           <ul class="list-group list-group-flush">
-            <li class="list-group-item fw-bolder">${e.Title}</li>
-            <li class="list-group-item">${e.Year}</li>
-          
-          </ul>
-        </div>`
-
-
-          
+        let x = "";
+        console.log(film)
+        film.forEach((e) => {
+          x += ` <div class="card mb-2" style="width: 18rem;">
+          <img src="${e.Poster}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${e.Title}</h5>
+            <p class="card-text">${e.Year}</p>
+            <a href="#" class="mdl-btn btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdb="${e.imdbID}">Go somewhere</a>
+          </div>
+        </div>
+      
+    `;
         });
 
-        let wadah = document.getElementById("pembungkus")
-        wadah.innerHTML=x
+        let wadah = document.getElementById("pembungkus");
+        wadah.innerHTML = x;
+
+        let modalbtn= document.querySelectorAll(".mdl-btn")
+        modalbtn.forEach((e)=>{
+          e.addEventListener("click",function(){
+            console.log(this.dataset.imdb)
+            let idfilm= this.dataset.imdb
+            fetch(`http://www.omdbapi.com/?apikey=13997e33&i=`+idfilm)
+            // fetch(`http://www.omdbapi.com/?apikey=13997e33&i=${idfilm}`)
+            .then(((e)=> e.json()))
+            .then((response)=>{ 
+
+              let isidetail = `
+              <div class="container-fluid">
+              <div class="col-2">
+                <img src="${response.Poster}"alt="...">
+              </div>
+              <div class="col">
+                <ul class="list-group">
+                  <li class="list-group-item">nama: muhammad </li>
+                  <li class="list-group-item">kitab suci alquran</li>
+                  <li class="list-group-item">islam</li>
+                  <li class="list-group-item">${response.Title}</li>
+                  <li class="list-group-item">${response.Plot}</li>
+                </ul>
+            </div>
+              `
+              let detailmdl = document.querySelector(".modal-body")
+
+              detailmdl.innerHTML= isidetail
+              
+              console.log(response)
+
+       
+
+            })
+          
+          
+          })
+        })
 
       })
-      .catch((response)=>{ alert(" judul yang anda cari belum tersedia di server kami\n silahkan cari judul lain")})
+      .catch((response) => {
+        alert(
+          " judul yang anda cari belum tersedia di server kami\n silahkan cari judul lain"
+        );
+      });
   }
 }
 
+let data = new api();
 
-let data = new api()
-
-
-export{data}
+export { data };
 // export let cont = document.getElementById("cari")
 // cont.addEventListener("click",()=>{alert("ok")})
- 
