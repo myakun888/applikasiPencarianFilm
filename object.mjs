b3c38c57dc8fe9a14c2f9e212449cebe79c126b6
muhammad fajrin <fajrinsaputra7@gmail.com>
2023-09-11 15:52:37 +0700

    merapikan detail data, dan sedkit merecfactoring file index dan object

--- object.mjs
+++ object.mjs
@@ -13,16 +13,16 @@ class api {
       .then((e) => e.json())
       .then((response) => {
         let film = response.Search;
-        // console.log(film);
+
         let x = "";
-        console.log(film)
+
         film.forEach((e) => {
           x += ` <div class="card mb-2" style="width: 18rem;">
           <img src="${e.Poster}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${e.Title}</h5>
             <p class="card-text">${e.Year}</p>
-            <a href="#" class="mdl-btn btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdb="${e.imdbID}">Go somewhere</a>
+            <a href="#" class="mdl-btn btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdb="${e.imdbID}">lihat detail</a>
           </div>
         </div>
       
@@ -32,45 +32,46 @@ class api {
         let wadah = document.getElementById("pembungkus");
         wadah.innerHTML = x;
 
-        let modalbtn= document.querySelectorAll(".mdl-btn")
-        modalbtn.forEach((e)=>{
-          e.addEventListener("click",function(){
-            console.log(this.dataset.imdb)
-            let idfilm= this.dataset.imdb
-            fetch(`http://www.omdbapi.com/?apikey=13997e33&i=`+idfilm)
-            // fetch(`http://www.omdbapi.com/?apikey=13997e33&i=${idfilm}`)
-            .then(((e)=> e.json()))
-            .then((response)=>{ 
-
-              let isidetail = `
+        let modalbtn = document.querySelectorAll(".mdl-btn");
+        modalbtn.forEach((e) => {
+          e.addEventListener("click", function () {
+            console.log(this.dataset.imdb);
+            let idfilm = this.dataset.imdb;
+            fetch(`http://www.omdbapi.com/?apikey=13997e33&i=` + idfilm)
+              // fetch(`http://www.omdbapi.com/?apikey=13997e33&i=${idfilm}`)
+              .then((e) => e.json())
+              .then((response) => {
+                let isidetail = `
               <div class="container-fluid">
-              <div class="col-2">
-                <img src="${response.Poster}"alt="...">
+              <row> 
+              <div class="col-md-3 bt-1">
+                <img src="${response.Poster}" class="img-fluid" alt="...">
               </div>
               <div class="col">
                 <ul class="list-group">
-                  <li class="list-group-item">nama: muhammad </li>
-                  <li class="list-group-item">kitab suci alquran</li>
-                  <li class="list-group-item">islam</li>
-                  <li class="list-group-item">${response.Title}</li>
-                  <li class="list-group-item">${response.Plot}</li>
+                  <li class="list-group-item"><strong>Title</strong>  : ${response.Title}</li>
+                  <li class="list-group-item"><strong>Genre</strong>  : ${response.Genre}</li>
+                  <li class="list-group-item"><strong>Actor/Actris</strong>  : ${response.Actors}</li>
+                  <li class="list-group-item"><strong>Bahasa</strong>  : ${response.Language}</li>
+                  <li class="list-group-item"><strong>Tahun</strong> :${response.Year}</li>
+                  <li class="list-group-item"><strong>Negara</strong>  : ${response.Country}</li>
+                  <li class="list-group-item"><strong>rating</strong> :${response.Ratings[0].Source} </br> ${response.Ratings[0].Value}
+
+                  </li>
+                  <li class="list-group-item"><strong>Plot</strong> :${response.Plot}</li>
+
                 </ul>
+                </row>
             </div>
-              `
-              let detailmdl = document.querySelector(".modal-body")
+              `;
+                let detailmdl = document.querySelector(".modal-body");
 
-              detailmdl.innerHTML= isidetail
-              
-              console.log(response)
+                detailmdl.innerHTML = isidetail;
 
-       
-
-            })
-          
-          
-          })
-        })
-
+                console.log(response);
+              });
+          });
+        });
       })
       .catch((response) => {
         alert(
@@ -85,3 +86,7 @@ let data = new api();
 export { data };
 // export let cont = document.getElementById("cari")
 // cont.addEventListener("click",()=>{alert("ok")})
+
+// <li class="list-group-item">nama: muhammad </li>
+// <li class="list-group-item">kitab suci alquran</li>
+// <li class="list-group-item">islam</li>
